f3332e44b5834a8b0445743ec72a0e75ab8941e8
JAlcocerT <jalcocert@fossengineer.com>
2024-07-09 13:59:15 +0200

    fixing gh actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,22 @@ import { defineConfig } from "astro/config";
 const BASE_PATH = "/slides-template";
 
 export default defineConfig({
-  site: "https://jalcocert.github.io",
+  site: 'https://jsulpis.github.io',
   base: BASE_PATH,
+  vite: {
+    build: {
+      rollupOptions: {
+        external: ['fsevents']
+      }
+    }
+  }
 });
+
+// export default defineConfig({
+//   site: "https://jalcocert.github.io",
+//   base: BASE_PATH,
+// });
+
+
 
 export { BASE_PATH };
